601142b7e891e1122768b073a06617c137262f04
ausaaf3108 <114802220+ausaaf3108@users.noreply.github.com>
2024-08-04 19:42:45 +0530

    use this instead of the other pkg

--- hand_ws/src/hand_pkg/hand_pkg/send_hand.py
+++ hand_ws/src/hand_pkg/hand_pkg/send_hand.py
@@ -2,11 +2,14 @@ import rclpy
 from rclpy.node import Node
 from .hand_test import HandDetect
 import cv2
+from geometry_msgs.msg import Twist
 
 class HandSend(Node):
 
     def __init__(self,name):
         super().__init__(name)
+        self.publisher_ = self.create_publisher(Twist,"micro_ros_arduino_twist_subscriber",10)
+
 
     def hand_draw(self):
         self.hd = HandDetect()
@@ -17,7 +20,15 @@ class HandSend(Node):
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-            self.get_logger().info(str(self.hd.state))
+            #self.get_logger().info(str(self.hd.state))
+            msg = Twist()
+            msg.linear.x = float(self.hd.state[0])
+            msg.linear.y = float(self.hd.state[1])
+            msg.linear.z = float(self.hd.state[2])
+            msg.angular.x = float(self.hd.state[3])
+            msg.angular.y = float(self.hd.state[4])
+            msg.angular.z = 0.0
+            self.publisher_.publish(msg)
             
         self.hd.cap.release()
         cv2.destroyAllWindows()
